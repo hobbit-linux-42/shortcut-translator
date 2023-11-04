b8fad2395021edba6a128b422ce67265b03dd41f
hobbit-linux-42 <149895107+hobbit-linux-42@users.noreply.github.com>
2023-11-04 18:35:47 +0100

    Add files via upload

---
+++ translate.py
@@ -0,0 +1,32 @@
+from googletrans import Translator, constants
+from subprocess import check_output
+from codecs import decode
+from tkinter import *
+
+def translator():
+    text=check_output(["xclip", "-o"]).decode()
+    trans = Translator()
+    translation = trans.translate(text, dest="it")
+    translation.text
+    count=0
+    formatted=""
+    for ch in list(translation.text):
+        count += 1
+        if count == 55:
+            count=0
+            formatted += "\n"
+        formatted += ch
+    return formatted
+
+def gui():
+    window = Tk()
+    window.geometry("400x400+55+400")
+    window.overrideredirect(True)
+    text=translator()
+    text_label = Label(window, text=text).place(x=0, y=0)
+    window.bind("<Button-1>", exit)
+    
+    window.mainloop()
+
+if __name__ == "__main__":
+    gui()
